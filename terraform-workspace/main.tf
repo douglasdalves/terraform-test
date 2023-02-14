c76d30c585cfa886bd5a47e4be8aa3fb7aa5b9d6
Douglas alves dos santos <dalves.info@gmail.com>
2023-02-14 20:26:30 -0300

    reordenação

---
+++ terraform-workspace/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">=1.2.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.17.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+  #backend "s3" {}
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+locals {
+  now         = timestamp()
+  brasilia_tz = timeadd(local.now, "-3h")
+  date_br     = formatdate("DD/MM/YYYY", local.brasilia_tz)
+}
+
+locals {
+  common_tags = {
+    CreatedDate = local.brasilia_tz
+  }
+}
+
+########################
+## Chamada de Modulos ##
+########################
+
+
+module "grupo_admin" {
+  source    = "../module-iam-group"
+  iam_dados = var.dados_modulo_iam
+  env       = var.enviroment
+}
---
+++ terraform-workspace/variables.tf
@@ -0,0 +1,73 @@
+
+variable "registro" {
+  type        = map(string)
+  description = "Tags do terraform para contas."
+
+  default = {
+    "Owner"     = "Cloud-test"
+    "project"   = "lab-stage"
+    "ManagedBy" = "Terraform"
+  }
+}
+
+variable "dados_modulo_iam" {
+  type = object({
+    hml = object({
+      group_name              = string
+      policy_name_admin       = string
+      policy_name_deny        = string
+      policy_name_billing     = string
+      policy_name_denny       = string
+      policy_name_iam_billing = string
+    })
+    prd = object({
+      group_name              = string
+      policy_name_admin       = string
+      policy_name_deny        = string
+      policy_name_billing     = string
+      policy_name_denny       = string
+      policy_name_iam_billing = string
+    })
+  })
+
+  default = {
+    hml = {
+      group_name              = "admin-testando"
+      policy_name_admin       = "AdminAccess"
+      policy_name_deny        = "SupportDeny-test"
+      policy_name_billing     = "BilDenyPolicy"
+      policy_name_denny       = "Support-denny-test",
+      policy_name_iam_billing = "Billing-test"
+    }
+    prd = {
+      group_name              = "admin-testando1"
+      policy_name_admin       = "AdminAccess1"
+      policy_name_deny        = "SupportDeny-test1"
+      policy_name_billing     = "BilDenyPolicy1"
+      policy_name_denny       = "Support-denny-test1",
+      policy_name_iam_billing = "Billing-test1"
+    }
+  }
+}
+
+#-----------------------------------------------------
+variable "aws_profile" {
+  type        = string
+  description = "Informar o profile configurado no aws cli"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Preecha a região a ser usada."
+}
+
+variable "enviroment" {
+  type        = string
+  description = "Digite o nome do ambiente que deseja executar o terraform (hml, dev, qa, stg ou prod)"
+}
+
+
+#https://medium.com/digitalproductsdev/organizando-sua-infraestrutura-como-c%C3%B3digo-com-terraform-df6ca70d8eb
+
+
+#-auto-approve -var-file="hml.tfvars
